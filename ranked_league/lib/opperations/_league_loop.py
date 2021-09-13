b68300f827aeae09d410dad496218e24a37b2ed1
pietabrood <pieter_nortje@outlook.com>
2021-09-13 19:57:44 +0200

    Development: Added sort funtion to League class

---
+++ ranked_league/lib/opperations/_league_loop.py
@@ -0,0 +1,21 @@
+
+def loop(file, _league):
+    for line in file:
+        teams = line.split(",")
+
+        teams[0]= teams[0].lstrip().split(" ")
+        teams[1]= teams[1].lstrip().split(" ")
+
+        score_1 = int(teams[0][len(teams[0]) - 1])
+        score_2 = int(teams[1][len(teams[1]) - 1])
+
+        if score_1 > score_2:
+            _league.trigger_win(teams[0][0])
+            _league.trigger_lose(teams[1][0])
+        
+        elif score_1 == score_2:
+            _league.trigger_draw([teams[0][0], teams[1][0]])
+
+        else:
+            _league.trigger_win(teams[1][0])
+            _league.trigger_lose(teams[0][0])
